f8af85d38a8603da9de9410c6bf6f8b912c5a1f2
Picao Baby <lioncubs11@gmail.com>
2021-07-09 18:06:41 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,541 @@
+var schedule = [
+	{
+		day: 1,
+		videos: [
+			{ 
+				basename: "1_out_3",
+				link: "videos/1_out_3.mp4",
+				image: "images/thumbs/1_out_3.jpg" 
+			},
+			{ 
+				basename: '5_out_3',
+				link: "https://youtu.be/2pLT-olgUJs",
+			  	image: "workout-abs.webp" 
+			},
+			{ 
+				basename: '1_out_10',
+				link: "https://youtu.be/L_xrDAtykMI",
+				image: "workout-flexibility.webp" 
+			}
+		]
+	},
+	{
+		day: 2,
+		videos: [
+			{ 
+				basename: "2_out_3",
+				link: "videos/1_out_3.mp4",
+				image: "images/thumbs/1_out_3.jpg" 
+			},
+			{ 
+				basename: '4_out_3',
+				link: "https://youtu.be/2pLT-olgUJs",
+			  	image: "workout-abs.webp" 
+			}
+		]
+	},
+	{
+		day: 3,
+		videos: [
+			{ 
+				basename: "3_out_2",
+				link: "videos/1_out_3.mp4",
+				image: "images/thumbs/1_out_3.jpg" 
+			}
+		]
+	},
+	{
+		day: 4,
+		videos: [
+			{ 
+				basename: "2_out_3",
+				link: "videos/2_out_3.mp4",
+				image: "images/thumbs/1_out_3.jpg" 
+			},
+			{ 
+				basename: '1_out_3',
+				link: "https://youtu.be/2pLT-olgUJs",
+			  	image: "workout-abs.webp" 
+			},
+			{ 
+				basename: '4_out_2',
+				link: "https://youtu.be/L_xrDAtykMI",
+				image: "workout-flexibility.webp" 
+			}
+		]
+	},
+	{
+		day: 5,
+		videos: [
+			{ 
+				basename: "2_out_3",
+				link: "videos/1_out_3.mp4",
+				image: "images/thumbs/1_out_3.jpg" 
+			},
+			{ 
+				basename: '5_out_2',
+				link: "https://youtu.be/2pLT-olgUJs",
+			  	image: "workout-abs.webp" 
+			}
+		]
+	},
+	{
+		day: 6,
+		videos: [
+			{ 
+				basename: "2_out_3",
+				link: "videos/1_out_3.mp4",
+				image: "images/thumbs/1_out_3.jpg" 
+			},
+			{ 
+				basename: '4_out_2',
+				link: "https://youtu.be/2pLT-olgUJs",
+			  	image: "workout-abs.webp" 
+			}
+		]
+	},
+	{
+		day: 7,
+		videos: [
+			{ 
+				basename: "1_out_3",
+				link: "videos/1_out_3.mp4",
+				image: "images/thumbs/1_out_3.jpg" 
+			}
+		]
+	},
+	{
+		day: 8,
+		videos: [
+			{ link: "https://youtu.be/EUruBzhv7Kk",
+			  image: "workout-slim-thigh.webp" },
+			{ link: "https://youtu.be/ZveSMkUr0l0",
+			  image: "workout-lower-abs.webp" },
+			{ link: "https://youtu.be/j64BBgBGNIU",
+			  image: "workout-toned-arms.webp" },
+		]
+	},
+	{
+		day: 9,
+		videos: [
+			{ link: "https://youtu.be/L_xrDAtykMI",
+			  image: "workout-flexibility.webp" },
+		]
+	},
+	{
+		day: 10,
+		videos: [
+			{ link: "https://youtu.be/2MoGxae-zyo",
+			  image: "workout-lose-weight.webp" },
+			{ link: "https://youtu.be/2pLT-olgUJs",
+			  image: "workout-abs.webp" },
+			{ link: "https://youtu.be/6TmQiugy_qw",
+			  image: "workout-line-abs.webp" },
+		]
+	},
+	{
+		day: 11,
+		videos: [
+			{ link: "https://youtu.be/EUruBzhv7Kk",
+			  image: "workout-slim-thigh.webp" },
+			{ link: "https://youtu.be/V4sWpLJcQoU",
+			  image: "workout-standing-abs.webp" },
+			{ link: "https://youtu.be/L_xrDAtykMI",
+			  image: "workout-flexibility.webp" },
+		]
+	},
+	{
+		day: 12,
+		videos: [
+			{ link: "https://youtu.be/2MoGxae-zyo",
+			  image: "workout-lose-weight.webp" },
+			{ link: "https://youtu.be/2pLT-olgUJs",
+			  image: "workout-abs.webp" },
+			{ link: "https://youtu.be/I9nG-G4B5Bs",
+			  image: "workout-lower-burn.webp" },
+		]
+	},
+	{
+		day: 13,
+		videos: [
+			{ link: "https://youtu.be/004CudS_3Ew",
+			  image: "workout-2000reps.webp" },
+			{ link: "https://youtu.be/L_xrDAtykMI",
+			  image: "workout-flexibility.webp" },
+		]
+	},
+	{
+		day: 14,
+		videos: [
+		]
+	},
+	{
+		day: 15,
+		videos: [
+			{ link: "https://youtu.be/2MoGxae-zyo",
+			  image: "workout-lose-weight.webp" },
+			{ link: "https://youtu.be/2pLT-olgUJs",
+			  image: "workout-abs.webp" },
+			{ link: "https://youtu.be/3Pr6n-nKfMA",
+			  image: "workout-core-arms.webp" },
+		]
+	},
+	{
+		day: 16,
+		videos: [
+			{ link: "https://youtu.be/2MoGxae-zyo",
+			  image: "workout-lose-weight.webp" },
+			{ link: "https://youtu.be/6TmQiugy_qw",
+			  image: "workout-line-abs.webp" },
+			{ link: "https://youtu.be/I9nG-G4B5Bs",
+			  image: "workout-lower-burn.webp" },
+			{ link: "https://youtu.be/L_xrDAtykMI",
+			  image: "workout-flexibility.webp" },
+		]
+	},
+	{
+		day: 17,
+		videos: [
+			{ link: "https://youtu.be/EUruBzhv7Kk",
+			  image: "workout-slim-thigh.webp" },
+			{ link: "https://youtu.be/V4sWpLJcQoU",
+			  image: "workout-standing-abs.webp" },
+			{ link: "https://youtu.be/j64BBgBGNIU",
+			  image: "workout-toned-arms.webp" },
+		]
+	},
+	{
+		day: 18,
+		videos: [
+			{ link: "https://youtu.be/ro0svIr1UTI",
+			  image: "workout-1000reps.webp" },
+			{ link: "https://youtu.be/L_xrDAtykMI",
+			  image: "workout-flexibility.webp" },
+		]
+	},
+	{
+		day: 19,
+		videos: [
+		]
+	},
+	{
+		day: 20,
+		videos: [
+			{ link: "https://youtu.be/2MoGxae-zyo",
+			  image: "workout-lose-weight.webp" },
+			{ link: "https://youtu.be/3Pr6n-nKfMA",
+			  image: "workout-core-arms.webp" },
+			{ link: "https://youtu.be/6TmQiugy_qw",
+			  image: "workout-line-abs.webp" },
+		]
+	},
+	{
+		day: 21,
+		videos: [
+			{ link: "https://youtu.be/2MoGxae-zyo",
+			  image: "workout-lose-weight.webp" },
+			{ link: "https://youtu.be/EUruBzhv7Kk",
+			  image: "workout-slim-thigh.webp" },
+			{ link: "https://youtu.be/2pLT-olgUJs",
+			  image: "workout-abs.webp" },
+			{ link: "https://youtu.be/L_xrDAtykMI",
+			  image: "workout-flexibility.webp" },
+		]
+	},
+	{
+		day: 22,
+		videos: [
+			{ link: "https://youtu.be/2MoGxae-zyo",
+			  image: "workout-lose-weight.webp" },
+			{ link: "https://youtu.be/EUruBzhv7Kk",
+			  image: "workout-slim-thigh.webp" },
+			{ link: "https://youtu.be/2pLT-olgUJs",
+			  image: "workout-abs.webp" },
+		]
+	},
+	{
+		day: 23,
+		videos: [
+			{ link: "https://youtu.be/ro0svIr1UTI",
+			  image: "workout-1000reps.webp" },
+			{ link: "https://youtu.be/L_xrDAtykMI",
+			  image: "workout-flexibility.webp" },
+		]
+	},
+	{
+		day: 24,
+		videos: [
+			{ link: "https://youtu.be/2MoGxae-zyo",
+			  image: "workout-lose-weight.webp" },
+			{ link: "https://youtu.be/2pLT-olgUJs",
+			  image: "workout-abs.webp" },
+			{ link: "https://youtu.be/I9nG-G4B5Bs",
+			  image: "workout-lower-burn.webp" },
+		]
+	},
+	{
+		day: 25,
+		videos: [
+			{ link: "https://youtu.be/L_xrDAtykMI",
+			  image: "workout-flexibility.webp" },
+		]
+	},
+	{
+		day: 26,
+		videos: [
+			{ link: "https://youtu.be/2MoGxae-zyo",
+			  image: "workout-lose-weight.webp" },
+			{ link: "https://youtu.be/2pLT-olgUJs",
+			  image: "workout-abs.webp" },
+			{ link: "https://youtu.be/3Pr6n-nKfMA",
+			  image: "workout-core-arms.webp" },
+		]
+	},
+	{
+		day: 27,
+		videos: [
+			{ link: "https://youtu.be/2MoGxae-zyo",
+			  image: "workout-lose-weight.webp" },
+			{ link: "https://youtu.be/V4sWpLJcQoU",
+			  image: "workout-standing-abs.webp" },
+			{ link: "https://youtu.be/ZveSMkUr0l0",
+			  image: "workout-lower-abs.webp" },
+		]
+	},
+	{
+		day: 28,
+		videos: [
+			{ link: "https://youtu.be/004CudS_3Ew",
+			  image: "workout-2000reps.webp" },
+			{ link: "https://youtu.be/L_xrDAtykMI",
+			  image: "workout-flexibility.webp" },
+		]
+	},
+	{
+		day: 29,
+		videos: [
+		]
+	},
+	{
+		day: 30,
+		videos: [
+			{ link: "https://youtu.be/EUruBzhv7Kk",
+			  image: "workout-slim-thigh.webp" },
+			{ link: "https://youtu.be/ZveSMkUr0l0",
+			  image: "workout-lower-abs.webp" },
+			{ link: "https://youtu.be/j64BBgBGNIU",
+			  image: "workout-toned-arms.webp" },
+			{ link: "https://youtu.be/L_xrDAtykMI",
+			  image: "workout-flexibility.webp" },
+		]
+	},
+];
+
+$(document).ready(function () {
+	var myvideo = document.getElementById('player');
+	var mysource = document.getElementById('player_source');
+
+
+	generateCalendar();
+	setStatus();
+
+	// modal 
+	setModal();
+
+	// plyr
+	initPlyr()
+});
+
+function initPlyr() {
+	// console.log('init plyr .....')
+	// js
+	var preloadedScript = document.createElement("script");
+	preloadedScript.src = "plyr/plyr.min.js";
+	document.body.appendChild(preloadedScript);
+	// css 
+	var preloadedStyle = document.createElement("link");
+	preloadedStyle.rel="stylesheet";
+	preloadedStyle.type="text/css";
+	preloadedStyle.href = "plyr/plyr.css";
+	preloadedStyle.media ="all";
+	document.body.appendChild(preloadedStyle);
+
+	// // this works
+	// const elem = document.getElementById('player')
+	// console.log(elem)
+	
+	// Error: jquery.min.js:2 jQuery.Deferred exception: Plyr is not defined ReferenceError: Plyr is not defined
+	const player = new Plyr('#player', {
+		hideControls: false,
+		disableContextMenu: false,
+		controls: [
+			'play-large', // The large play button in the center
+			'restart', // Restart playback
+			'rewind', // Rewind by the seek time (default 10 seconds)
+			'play', // Play/pause playback
+			'fast-forward', // Fast forward by the seek time (default 10 seconds)
+			'progress', // The progress bar and scrubber for playback and buffering
+			'current-time', // The current time of playback
+			'duration', // The full duration of the media
+			'mute', // Toggle mute
+			'volume', // Volume control
+			'captions', // Toggle captions
+			'settings', // Settings menu
+			'pip', // Picture-in-picture (currently Safari only)
+			'airplay', // Airplay (currently Safari only)
+			'download', // Show a download button with a link to either the current source or a custom URL you specify in your options
+			'fullscreen', // Toggle fullscreen
+		]
+	});
+	console.log('done .... ')
+	console.log(player)	
+}
+
+function setModal() {
+	var myvideo = document.getElementById('player');
+	var mysource = document.getElementById('player_source');
+	mysource.setAttribute('src', '/videos/4_out_3.mp4');
+	myvideo.load();
+	myvideo.play();
+
+	const modal = document.querySelector(".modal");
+	const trigger = document.querySelector(".trigger");
+	const thumbs = document.querySelectorAll(".workout__thumb");
+	const closeButton = document.querySelector(".close-button");
+
+	console.log(thumbs)
+
+	function toggleModal() {
+		modal.classList.toggle("show-modal");
+	}
+
+	function windowOnClick(event) {
+		if (event.target === modal) {
+			toggleModal();
+		}
+	}
+
+	trigger.addEventListener("click", toggleModal);
+	thumbs.forEach( function(thumb) {
+		console.log('one thumb' + thumb) 	// thumb is 'http://.....mp4'
+		
+	})	
+	closeButton.addEventListener("click", toggleModal);
+	window.addEventListener("click", windowOnClick);
+}
+
+function generateCalendar() {
+	var scheduleList = $(".js-schedule-list");
+
+	$.each(schedule, function (key, value) {
+		var day = value.day;
+		var videos = value.videos;
+		var status = value.status;
+		if (videos.length == 0) {
+			var restHTML =
+				"<div class='js-day' data-status='false'><div class='workout__rest'>REST</div></div>";
+			scheduleList.append(restHTML);
+		} else if (videos.length > 0) {
+			
+			var allVideos = "";
+			var grammar = (videos.length === 1) ? 'video' :'videos';
+			
+			$.map(videos, function (key, value) {
+				// var videoHTML = '<a href="' + key.link + '" target="_blank">'+ '<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/567707/' + key.image + '" alt="Workout Video Image"/></a>';
+				var videoHTML = '<a class="workout__thumb" href="/videos/' + key.basename + '.mp4" target="_blank">'+ '<img src="/images/thumbs/' + key.basename + '.jpg" data-basename="HERE HERE" alt="'+ key.basename +'" /></a>';
+				allVideos = allVideos + videoHTML;
+			});
+			
+			var workHTML =
+				"<div class='js-day' data-status='false'><div class='workout__info'>" +
+				videos.length + ' ' + grammar + 
+				"</div><div class='workout__links'>"+ allVideos + "</div></div>";
+			
+			scheduleList.append(workHTML);
+			
+		}
+	});
+}
+
+function setStatus() {
+
+	$(".js-did-workout").on("click", function () {
+		var currentDay = $(".js-schedule-list").find(
+			'.js-day[data-status="false"]:first'
+		);
+		currentDay.attr("data-status", "success");
+		schedule.shift();
+		checkNextDay();
+	});
+
+	$(".js-no-workout").on("click", function () {
+		var currentDay = $(".js-schedule-list").find(
+			'.js-day[data-status="false"]:first'
+		);
+		currentDay.attr("data-status", "failed");
+		var originalDay = currentDay.data("day");
+		updateCalendar(originalDay);
+		updateDays();
+		checkNextDay();
+	});
+
+	// each thumb, add click handler, to open modal
+	$(".workout__thumb").on('click', function(e) {
+		e.preventDefault();
+
+		// console.log('this ----' + this) // this is not a, or img, but "http://....mp4"
+		var alt = e.target.alt;
+		console.log('alt ----- ' + alt);
+
+		
+	})
+}
+œ
+function updateCalendar(day) {
+	var scheduleList = $(".js-schedule-list");
+	scheduleList.append('<div class="js-day" data-status="false"></div>');
+
+	$.each(schedule, function (key, value) {
+		var day = value.day;
+		var videos = value.videos;
+		
+		var daysDiv = $('[data-status="false"]').eq(key);
+		
+		if (videos.length == 0) {
+			$(daysDiv).html("<div class='workout__rest'>REST</div></div>");
+		} else if (videos.length > 0) {
+			
+			var allVideos = "";
+			var grammar = (videos.length === 1) ? 'video':'videos';
+			
+			$.map(videos, function (key, value) {
+				var videoHTML = '<a href="' + key.link + '" target="_blank">'+ '<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/567707/' + key.image + '" alt="Workout Video Image"/></a>';
+				allVideos = allVideos + videoHTML;
+			});
+			
+			$(daysDiv).html("<div class='workout__info'>" +
+				videos.length + ' ' + grammar +
+				"</div><div class='workout__links'>" + allVideos + "</div>");
+		}
+	});
+}
+
+function updateDays() {
+	$(".js-day-count").text($(".js-day").length);
+}
+
+
+function checkNextDay() {
+	var dayAfter =  $('.js-day[data-status="false"]').first();
+	
+	if ($('.js-day[data-status="false"]').length == 0) {
+		$('.rest-day, .workout-day').css('display', 'none');
+		$('.amazing-day').css('display','flex'); 
+	} else if ($('.workout__rest', dayAfter).length) {
+		$('.rest-day').css('display', 'flex');
+		$('.workout-day').css('display','none');
+	} else {
+		$('.rest-day').css('display','none');
+		$('.workout-day').css('display', 'flex');
+	}
+}
